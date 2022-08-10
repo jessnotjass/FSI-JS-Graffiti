7c5011d7d0b4c666f14841a5e9d4c19993288b58
Jess <jevi.lanchinebre@gmail.com>
2022-08-09 18:58:11 -0700

    Activity Solution

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Select some elements...
+let header = document .querySelector('#page-header')
+let dogImages = document.querySelectorAll('.dog-image')
+let dogNames = document.querySelectorAll('.dog-name')
+let footer = document.querySelector('.footer')
+
+header.style.textAlign = 'left'
+
+for (let i = 0; i < dogImages.length; i++) {
+    dogImages[i].style.borderRadius = '50px'
+    dogImages[i].style.transform = 'scale(-1)'
+}
+
+for (let i = 0; i < dogNames.length; i++) {
+    console.log(dogNames[i])
+    dogNames[i].style.textAlign = 'left'
+}
+
+footer.style.color = 'red'
+footer.style.borderStyle = 'solid'
